4cef88069a90f507f587af5f591ead26287c85e1
rjmasache <royerjmasache@gmail.com>
2024-06-17 17:12:48 -0500

    Dependencias para formateo de Tailwind CSS

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,20 +1,20 @@
 import safetiveLogo from "../assets/safetive-logo.svg";
 
 export default function Navigation() {
-    return (
-        <div className="bg-navbar h-16 w-full flex flex-wrap flex-row justify-evenly items-center">
-            <a href="#" className="text-link font-comic">
-                <img src={safetiveLogo} alt="Safetive" className="w-14" />
-            </a>
-            <a className="text-link font-comic" href="#">
-                Inicio
-            </a>
-            <a className="text-link font-comic" href="#">
-                Cuentos
-            </a>
-            <a className="text-link font-comic" href="#">
-                Sobre nosotros
-            </a>
-        </div>
-    );
+  return (
+    <div className="flex h-16 w-full flex-row flex-wrap items-center justify-evenly bg-navbar">
+      <a href="#" className="font-comic text-link">
+        <img src={safetiveLogo} alt="Safetive" className="w-14" />
+      </a>
+      <a className="font-comic text-link" href="#">
+        Inicio
+      </a>
+      <a className="font-comic text-link" href="#">
+        Cuentos
+      </a>
+      <a className="font-comic text-link" href="#">
+        Sobre nosotros
+      </a>
+    </div>
+  );
 }
