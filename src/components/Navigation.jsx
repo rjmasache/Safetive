299230b94ea7cddeda9e7d45a731f6b514055430
rjmasache <royerjmasache@gmail.com>
2024-06-17 13:23:26 -0500

    Adición del Logo en el menú de navegación, eliminación de archivos innecesarios

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,11 @@
+import safetiveLogo from "../assets/safetive-logo.svg";
+
 export default function Navigation() {
     return (
         <div className="bg-navbar h-16 w-full flex flex-wrap flex-row justify-evenly items-center">
+            <a href="#" className="text-link font-comic">
+                <img src={safetiveLogo} alt="Safetive" className="w-14" />
+            </a>
             <a className="text-link font-comic" href="#">
                 Inicio
             </a>
