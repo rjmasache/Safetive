97c282d7dc3390562b2dc2330f964ea99d35617b
rjmasache <royerjmasache@gmail.com>
2024-06-17 17:33:33 -0500

    Menú de navegación para dispositivos móviles

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,20 +1,33 @@
 import safetiveLogo from "../assets/safetive-logo.svg";
+import mobileMenu from "../assets/mobile-menu.svg";
 
 export default function Navigation() {
-  return (
-    <div className="flex h-16 w-full flex-row flex-wrap items-center justify-evenly bg-navbar">
-      <a href="#" className="font-comic text-link">
-        <img src={safetiveLogo} alt="Safetive" className="w-14" />
-      </a>
-      <a className="font-comic text-link" href="#">
-        Inicio
-      </a>
-      <a className="font-comic text-link" href="#">
-        Cuentos
-      </a>
-      <a className="font-comic text-link" href="#">
-        Sobre nosotros
-      </a>
-    </div>
-  );
+    return (
+        <div className="flex h-16 w-full flex-row flex-wrap items-center justify-evenly bg-navbar">
+            <a href="#" className="font-comic text-link">
+                <img src={safetiveLogo} alt="Safetive" className="w-14" />
+            </a>
+            <a
+                className="invisible font-comic md:visible md:text-link"
+                href="#"
+            >
+                Inicio
+            </a>
+            <a
+                className="invisible font-comic md:visible md:text-link"
+                href="#"
+            >
+                Cuentos
+            </a>
+            <a
+                className="invisible font-comic md:visible md:text-link"
+                href="#"
+            >
+                Sobre nosotros
+            </a>
+            <button>
+                <img src={mobileMenu} alt="Menu" className="w-14 md:hidden" />
+            </button>
+        </div>
+    );
 }
