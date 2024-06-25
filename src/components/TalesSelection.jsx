38b72f24acf30bfd85a9a59f692fafb053fab9f7
rjmasache <royerjmasache@gmail.com>
2024-06-25 12:51:27 -0500

    Desarrollo de página de cuentos

---
+++ src/components/TalesSelection.jsx
@@ -0,0 +1,28 @@
+import familiar from "../assets/Familiar.jpg";
+import scholar from "../assets/Educative.jpg";
+import select from "../assets/Select.png";
+
+export default function TalesSelection() {
+    return (
+        <>
+            <section className="flex w-full flex-col sm:flex-row">
+                <TalesSelectionItem category={familiar} type={"familiar"} />
+                <TalesSelectionItem category={scholar} type={"escolar"} />
+            </section>
+            <div className="flex w-full items-center justify-center p-5">
+                <img src={select} alt="Select" className="w-1/2 sm:w-1/5" />
+            </div>
+        </>
+    );
+}
+
+export function TalesSelectionItem({ category, type }) {
+    return (
+        <div className="flex h-full w-full flex-col flex-wrap items-center justify-evenly">
+            <img src={category} alt="Contexto familiar" className="w-3/5 p-5" />
+            <button className="rounded-2xl bg-safetive p-3 font-comic font-black text-link">
+                Aventura {type}
+            </button>
+        </div>
+    );
+}
--- src/routes/Tales.jsx
+++ src/routes/Tales.jsx
@@ -1,9 +1,13 @@
+import Footer from "../components/Footer";
 import Navigation from "../components/Navigation";
+import TalesSelection from "../components/TalesSelection";
 
 export default function Tales() {
     return (
-        <>
+        <div className="flex min-h-screen w-full flex-col">
             <Navigation />
-        </>
+            <TalesSelection />
+            <Footer />
+        </div>
     );
 }
