c4e9bd083c6d5631ffda2ebabc38dd4a52136ab4
rjmasache <royerjmasache@gmail.com>
2024-06-28 12:08:41 -0500

    Desarrollo de interactividad para el botón principal

--- src/components/MainButton.jsx
+++ src/components/MainButton.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function MainButton() {
     return (
         <div className="flex h-24 w-full items-center justify-center">
             <button className="rounded-2xl bg-safetive p-3 font-comic font-black text-link">
-                ¡Empezar la aventura!
+                <Link to={`/tales`}>¡Empezar la aventura!</Link>
             </button>
         </div>
     );
