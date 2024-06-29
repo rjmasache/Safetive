2d7363639221108150e656470842229492a390f0
rjmasache <royerjmasache@gmail.com>
2024-06-29 18:27:24 -0500

    Desarrollo de galería de imágenes de la sección Sobre nosotros

--- src/assets/gallery.js
+++ src/assets/gallery.js
@@ -4,6 +4,8 @@ import image3 from "./hero-3.jpg";
 import image4 from "./hero-4.jpg";
 import image5 from "./other-hero-1.jpg";
 import image6 from "./other-hero-2.jpg";
+import picture1 from "./picture-1.jpg";
+import picture2 from "./picture-2.jpg";
 
 export const herosGallery = [
     {
@@ -47,4 +49,17 @@ export const otherHeroesGallery = [
     },
 ];
 
-export const aboutGallery = [];
+export const aboutGallery = [
+    {
+        id: 7,
+        name: "Picture 1",
+        src: picture1,
+        alt: "Seven",
+    },
+    {
+        id: 8,
+        name: "Picture 2",
+        src: picture2,
+        alt: "Eight",
+    },
+];
--- src/components/GalleryAboutUs.jsx
+++ src/components/GalleryAboutUs.jsx
@@ -1,44 +1,79 @@
-import test from "../assets/wallpaper.jpg";
+import { useState } from "react";
+import { aboutGallery } from "../assets/gallery.js";
 
 export default function GalleryAboutUs() {
+    const [currentImage, setCurrentImage] = useState(0);
+
+    function handleNextClick() {
+        setCurrentImage(currentImage + 1);
+
+        if (currentImage >= aboutGallery.length - 1) {
+            setCurrentImage(0);
+        }
+    }
+
+    function handlePreviousClick() {
+        setCurrentImage(currentImage - 1);
+
+        if (currentImage <= 0) {
+            setCurrentImage(aboutGallery.length - 1);
+        }
+    }
+
+    let picture = aboutGallery[currentImage];
+
     return (
-        <section className="flex w-full items-center justify-center p-10">
-            <div className="m-2 flex w-1/2 items-center justify-center">
-                <button className="inset-y-0 left-0 top-0 m-2 flex size-4 items-center justify-center rounded-full bg-slate-200 sm:m-4 sm:size-6">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="size-2 font-bold text-white sm:size-4"
+        <section className="mt-20 flex w-full items-center justify-center">
+            <div className="flex h-full w-10/12 items-center justify-center">
+                <div className="flex h-full items-center justify-center">
+                    <button
+                        className="inset-y-0 left-0 top-0 m-2 flex size-4 items-center justify-center rounded-full bg-slate-200 sm:m-4 sm:size-6"
+                        onClick={handlePreviousClick}
                     >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M15.75 19.5 8.25 12l7.5-7.5"
-                        />
-                    </svg>
-                </button>
-
-                <img src={test} alt="Test" className="w-10/12" />
-
-                <button className="inset-y-0 right-0 top-0 m-2 flex size-4 items-center justify-center rounded-full bg-slate-200 sm:m-4 sm:size-6">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="size-2 font-bold text-white sm:size-4"
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="size-2 font-bold text-white sm:size-4"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M15.75 19.5 8.25 12l7.5-7.5"
+                            />
+                        </svg>
+                    </button>
+                </div>
+                <div className="flex h-full basis-2/3 items-center justify-center">
+                    <img
+                        src={picture.src}
+                        alt={picture.name}
+                        className="h-full w-full"
+                    />
+                </div>
+                <div className="flex h-full items-center justify-center">
+                    <button
+                        className="inset-y-0 right-0 top-0 m-2 flex size-4 items-center justify-center rounded-full bg-slate-200 sm:m-4 sm:size-6"
+                        onClick={handleNextClick}
                     >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="m8.25 4.5 7.5 7.5-7.5 7.5"
-                        />
-                    </svg>
-                </button>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="size-2 font-bold text-white sm:size-4"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                            />
+                        </svg>
+                    </button>
+                </div>
             </div>
         </section>
     );
