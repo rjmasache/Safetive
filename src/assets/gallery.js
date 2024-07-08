42cf561ca5dfa4956c7228bbe6ad9a188f091031
rjmasache <royerjmasache@gmail.com>
2024-07-08 16:53:41 -0500

    Adición de imágenes para las opciones en el cuento familiar

--- src/assets/gallery.js
+++ src/assets/gallery.js
@@ -11,6 +11,13 @@ import familiarImage2 from "./familiar-image-2.jpeg";
 import familiarImage3 from "./familiar-image-3.jpeg";
 import familiarImage4 from "./familiar-image-4.jpg";
 import familiarImage5 from "./familiar-image-5.jpeg";
+import firstOptionImage1 from "./first-option-image-1.jpg";
+import firstOptionImage2 from "./first-option-image-2.jpeg";
+import firstOptionImage3 from "./first-option-image-3.jpg";
+import secondOptionImage1 from "./second-option-image-1.jpeg";
+import secondOptionImage2 from "./second-option-image-2.jpeg";
+import secondOptionImage3 from "./second-option-image-3.jpeg";
+import secondOptionImage4 from "./second-option-image-4.jpeg";
 
 export const herosGallery = [
     {
@@ -98,5 +105,79 @@ export const familiarTaleImages = [
         src: familiarImage4,
         otherSrc: familiarImage5,
         alt: "Twelve",
+    },
+];
+
+export const firstOptionTale = [
+    {
+        id: 1,
+        title: "APRENDIENDO A VOLAR",
+        text: "Bruce respondió: Sí, pero quería contarles algo y accidentalmente rompí el adorno favorito de mamá",
+        src: familiarImage4,
+        alt: "Thirteen",
+    },
+    {
+        id: 2,
+        title: "APRENDIENDO A VOLAR",
+        text: "Sus padres le dijeron: No te preocupes hijo, lo importante es que no te hayas lastimado. El adorno se puede arreglar después",
+        src: firstOptionImage1,
+        alt: "Fourteen",
+    },
+    {
+        id: 3,
+        title: "APRENDIENDO A VOLAR",
+        text: "Bruce se sintió tranquilo y se disculpó con sus padres. Después, junto con ellos recogieron con mucho cuidado los trozos del adorno",
+        src: firstOptionImage2,
+        alt: "Fifteen",
+    },
+    {
+        id: 4,
+        title: "APRENDIENDO A VOLAR",
+        text: `Al terminar, su papá le preguntó: ¿Qué nos querías contar hijo?
+        Bruce le dijo: Vamos al patio para mostrarles lo que puedo hacer.
+        Ellos lo siguieron y él de inmediato empezó a volar. Sus padres estuvieron muy felices porque su hijo les enseñó lo que aprendió y por el gran esfuerzo que hizo para lograrlo`,
+        src: firstOptionImage3,
+        alt: "Sixteen",
+    },
+];
+
+export const secondOptionTale = [
+    {
+        id: 1,
+        title: "APRENDIENDO A VOLAR",
+        text: `Papá gritando dijo: ¿Qué fue lo que hiciste?
+        Bruce asustado respondió: Accidentalmente rompí un adorno`,
+        src: familiarImage5,
+        alt: "Seventeen",
+    },
+    {
+        id: 2,
+        title: "APRENDIENDO A VOLAR",
+        text: `Su madre enojada respondió: ¡Es mi adorno favorito! ¡¿Por qué no tienes cuidado al hacer las cosas?!`,
+        src: secondOptionImage1,
+        alt: "Eighteen",
+    },
+    {
+        id: 3,
+        title: "APRENDIENDO A VOLAR",
+        text: "Entonces sus padres lo castigaron, hicieron que Bruce sienta dolor y miedo por sus gritos",
+        src: secondOptionImage2,
+        alt: "Nineteen",
+    },
+    {
+        id: 4,
+        title: "APRENDIENDO A VOLAR",
+        text: `Al día siguiente el profesor de Bruce notó que él estaba triste, así que le preguntó: ¿Qué te pasa, Bruce?
+        Bruce, muy triste, le respondió: Me siento mal porque mamá y papá me castigaron muy feo y no es la primera vez
+        El profesor le dijo: Lo siento mucho, Bruce. Nadie debe hacerte daño, por eso es muy importante que le cuentes a alguien como yo para poder ayudarte a encontrar una solución`,
+        src: secondOptionImage3,
+        alt: "Twenty",
+    },
+    {
+        id: 5,
+        title: "APRENDIENDO A VOLAR",
+        text: "Luego el profesor habló con los padres de Bruce y poco a poco la casa de Bruce se convirtió en un lugar más tranquilo y feliz, donde todos podían cometer errores y aprender de ellos sin miedo",
+        src: secondOptionImage4,
+        alt: "Twenty-one",
     },
 ];
