780b8d65002b42db663c96c75dc2ece077a96aab
rjmasache <royerjmasache@gmail.com>
2024-07-09 14:13:32 -0500

    Corrección de sección de imagen principal

--- src/components/Wallpaper.jsx
+++ src/components/Wallpaper.jsx
@@ -1,7 +1,7 @@
 export default function Wallpaper() {
     return (
         <section className="flex h-72 w-full flex-wrap items-start justify-center bg-wallpaper bg-cover bg-center bg-no-repeat sm:h-custom">
-            <div className="m-3 flex h-1/4 w-1/4 flex-wrap items-center justify-center">
+            <div className="m-5 flex w-2/4 flex-wrap items-center justify-center sm:w-1/4">
                 <p className="text-center font-comic text-xs font-bold text-safetive sm:text-lg">
                     Explora y descubre cómo los pequeños héroes superan cada
                     desafío en nuestro cuento de aventuras
